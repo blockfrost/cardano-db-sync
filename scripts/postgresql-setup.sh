56d3b48aa3d0956521a804294660596674843772
Michael Bishop <cleverca22@gmail.com>
2025-03-11 01:25:15 +0000

    adjust the output path

--- scripts/postgresql-setup.sh
+++ scripts/postgresql-setup.sh
@@ -168,18 +168,19 @@ function create_snapshot {
 	  lstate_gz_file=$(basename "${ledger_file}").gz
 	  gzip --to-stdout "${ledger_file}" > "${tmp_dir}/${lstate_gz_file}"
 	fi
+	tree "${tmp_dir}"
 	# Use plain tar here because the database dump files and the ledger state file are already gzipped. Disable Shellcheck SC2046 to avoid empty '' getting added while quoting
 	# shellcheck disable=SC2046
-	tar cvf - --directory "${tmp_dir}" "db" $( [ -n "${lstate_gz_file:-}" ] && [ -f "/${tmp_dir}/${lstate_gz_file}" ] && echo "${lstate_gz_file}" ) | tee "${tgz_file}.tmp" | \
-	  sha256sum | head -c 64 | sed -e "s/$/  ${tgz_file}\n/" > "${tgz_file}.sha256sum"
-	mv "${tgz_file}.tmp" "${tgz_file}"
+	tar cvf - --directory "${tmp_dir}" "db" $( [ -n "${lstate_gz_file:-}" ] && [ -f "/${tmp_dir}/${lstate_gz_file}" ] && echo "${lstate_gz_file}" ) | tee "/var/lib/postgresql/tmp/${tgz_file}.tmp" | \
+	  sha256sum | head -c 64 | sed -e "s/$/  ${tgz_file}\n/" > "/var/lib/postgresql/tmp/${tgz_file}.sha256sum"
+	mv "/var/lib/postgresql/tmp/${tgz_file}.tmp" "/var/lib/postgresql/tmp/${tgz_file}"
 	rm "${recursive}" "${force}" "${tmp_dir}"
-	if test "$(tar "${test}" --file "${tgz_file}")" ; then
+	if test "$(tar "${test}" --file "/var/lib/postgresql/tmp/${tgz_file}")" ; then
 	  echo "Tar reports the snapshot file as being corrupt."
 	  echo "It is not safe to drop the database and restore using this file."
 	  exit 1
 	fi
-	echo "Created ${tgz_file} + .sha256sum"
+	echo "Created /var/lib/postgresql/tmp/${tgz_file} + .sha256sum"
 }
 
 function restore_snapshot {
